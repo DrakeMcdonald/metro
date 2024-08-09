00558089825d6cd4e53ee3c860efd5a60a751cd3
DrakeMcdonald <drake.mcdonald078@gmail.com>
2024-08-09 07:21:11 +0000

    initial code

---
+++ node.js
@@ -0,0 +1,10 @@
+let metroCard = "metroCard"
+let fee = "fee"
+fee = 2.75
+metroCard = 5
+if (metroCard > fee) {
+   console.log("I have enough money") 
+}
+else {
+ console.log("darn, guess I have to walk")
+}
